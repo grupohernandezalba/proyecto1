e508d3f48fdc3b07f22f40c24e8e451b6961e76a
Marco Antonio Hernandez Alba <marco@grupohernandezalba.com>
2025-03-06 22:02:20 -0600

    final

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://proyecto1astro.netlify.app"
+  site: "https://proyecto1astro.netlify.app",
+  integrations: [preact()]
 });
